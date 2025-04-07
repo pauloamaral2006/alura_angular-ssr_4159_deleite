a13e7bf36048ac157126f158865ef4e6fa304dd5
Paulo Amaral <pauloamaral2006@yahoo.com.br>
2025-04-07 15:36:29 -0300

    Aula 01

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+
+import { createClient } from '@supabase/supabase-js';
+import { environment } from '../../environments/environment.development';
+import { from, Observable } from 'rxjs';
+import { Product } from '../interfaces/product';
+
+const supabase = createClient(environment.supabaseUrl, environment.supabaseKey);
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  constructor() {}
+
+  getProducts(): Observable<Product[]> {
+    return from(
+      supabase
+        .from('products')
+        .select('*')
+        .then(({ data: products, error }) => {
+          if (error) throw new Error(error.message);
+          return products || [];
+        })
+    );
+  }
+
+  getProductById(id: number): Observable<Product> {
+    return from(
+      supabase
+        .from('products')
+        .select('*')
+        .eq('id', id)
+        .single()
+        .then(({ data: product, error }) => {
+          if (error) throw new Error(error.message);
+          return product;
+        })
+    );
+  }
+}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://cmsqttyiggsyblyqptzv.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNtc3F0dHlpZ2dzeWJseXFwdHp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQwNDg3MTEsImV4cCI6MjA1OTYyNDcxMX0.vD-vYF-S44C4-2uB4W8atjDgp33rxaLVyTJ_AXYzlAg',
+};
