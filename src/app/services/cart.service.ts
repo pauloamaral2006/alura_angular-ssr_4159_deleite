b6e8d9024e60400962f736b47693f2236a9e1b53
Paulo Amaral <pauloamaral2006@yahoo.com.br>
2025-04-15 16:51:40 -0300

    Curso 02 - Aula 03

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, from, Observable } from 'rxjs';
 import { CartItem } from '../interfaces/cart_item';
 import { Product } from '../interfaces/product';
+import { supabase } from './supabase.client';
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +11,42 @@ export class CartService {
   private cartItemsSubject = new BehaviorSubject<CartItem[]>([]);
   cartItem$ = this.cartItemsSubject.asObservable();
 
+  constructor() {
+    this.loadCart();
+  }
+
+  private loadCart() {
+    from(
+      supabase
+        .from('cart_items')
+        .select('quantity, product:product_id (id, title, price, image)')
+    ).subscribe({
+      next: ({ data, error }) => {
+        if (error) {
+          console.error('Erro ao carregar o carrinho', error.message);
+        } else {
+          const items = (data || []).map((item: any) => ({
+            product: item.product,
+            quantity: item.quantity,
+          }));
+          this.cartItemsSubject.next(items);
+        }
+      },
+    });
+  }
+  private saveCartitem(cartItem: CartItem): Observable<void> {
+    return from(
+      supabase
+        .from('cart_items')
+        .upsert({
+          product_id: cartItem.product.id,
+          quantity: cartItem.quantity,
+        })
+        .then(({ error }) => {
+          if (error) throw new Error(error.message);
+        })
+    );
+  }
   private getCurrentItems(): CartItem[] {
     return this.cartItemsSubject.getValue();
   }
@@ -27,6 +64,9 @@ export class CartService {
     }
 
     this.cartItemsSubject.next(currentItems);
+    this.saveCartitem(
+      currentItems[itemIndex] || { product, quantity }
+    ).subscribe({});
   }
 
   getTotalQuantity(): number {
